bf68e4b334ee0c0ea57d3ab4c2d27e46ab5ef547
maleeshaLH <lhmv114@gmil.com>
2025-01-22 23:09:39 +0530

    manager Customer Routers

--- database/prisma-data.ts
+++ database/prisma-data.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import {Customer} from "../model/customer";
+import router from "../router/customerRouter";
 
 const prisma = new PrismaClient()
 
@@ -19,4 +20,40 @@ export async function addCustomer(customer :Customer){
        console.log(error)
    }
 
+}
+
+export async function getAllCustomer(){
+    try {
+        return await prisma.customer.findMany()
+
+    }catch (error){
+        return error;
+    }
+}
+
+export async function deleteCustomer(id:number){
+    try {
+        await prisma.customer.delete({
+            where:{id: id}
+        })
+        return 'Customer Delete Successfully'
+    }catch (error){
+        return error
+    }
+}
+export async function updateCustomer(id: number, customer: Customer){
+    try{
+        await prisma.customer.update({
+            where:{ id : id},
+            data:{
+                name: customer.name,
+                age: customer.age,
+                address: customer.address
+            }
+        })
+        return 'Update success'
+    }catch(err){
+        console.log("error updating customer", err);
+        return  err
+    }
 }
--- router/customerRouter.ts
+++ router/customerRouter.ts
@@ -3,15 +3,40 @@ import {
     getCustomer
 } from "../database/localDateStore";
  import  express from 'express'
-import {addCustomer} from "../database/prisma-data";
+import {addCustomer, deleteCustomer, getAllCustomer, updateCustomer} from "../database/prisma-data";
+import {Customer} from "../model/customer";
 const router = express.Router();
 
  router.post('/post',async (req, res) => {
      res.send( await addCustomer(req.body))
  })
 
-router.get('/get',(req, res) => {
-    res.send(getCustomer())
+router.get('/get',async (req, res) => {
+    try {
+        res.json(await getAllCustomer())
+    }catch (error){
+        console.log('error get all customer')
+    }
+})
+router.delete("/delete/:id", async (req, res) => {
+    const id: number  =+ req.params.id;
+    try{
+        await deleteCustomer(id);
+        res.send('Customer Deleted');
+    }catch(err){
+        console.log("error deleting customer", err);
+    }
+})
+router.put("/update/:id",async (req, res) => {
+    const id:number = +req.params.id;
+    const customer : Customer = req.body;
+
+    try{
+        await updateCustomer(id, customer);
+        res.send('Customer Updated');
+    }catch(err){
+        console.log("error updating customer", err);
+    }
 })
 
 export default router;
